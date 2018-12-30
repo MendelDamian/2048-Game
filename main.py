7f240db19b97fd5242262c23511699a020652bbb
Damian Mendel <mendeldamian01@gmail.com>
2018-12-30 17:21:47 +0100

    Drawing fields - finished

--- main.py
+++ main.py
@@ -3,19 +3,22 @@ import Square
 import pygame
 
 screen = Screen.Screen()
-size = 2
-squares =  []
-w1 = screen.screen_width/2
-squares.append(Square.Square(x1=20, y1=20, x2=365, y2=365)) #11
-squares.append(Square.Square(x1=405, y1=20, x2=365, y2=365)) #12
-squares.append(Square.Square(x1=20, y1=405, x2=365, y2=365)) #21
-squares.append(Square.Square(x1=405, y1=405, x2=365, y2=365)) #22
+size = 4
+width = (screen.screen_width-40-10*(2*size-1))/size
+squares = []
+
+for i in range(size):
+    for j in range(size):
+        x1 = j*20+20
+        y1 = i*20+20
+        squares.append(Square.Square(x1=x1+j*width, y1=y1+i*width,
+                                     x2=width, y2=width))
 
 while True:
     screen.event_catcher()
     for i in range(size*size):
-        pygame.draw.rect(screen.screen, (i+75, i+50, i+100), [squares[i].x1, squares[i].y1,
-                                                              squares[i].x2, squares[i].y2])
+        pygame.draw.rect(screen.screen, (200, 200, 100), [squares[i].x1, squares[i].y1,
+                                                          squares[i].x2, squares[i].y2])
 
     pygame.display.update()
     screen.clock.tick(60)
